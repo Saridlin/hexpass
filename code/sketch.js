62886907c36726be873f1fc6a04f91008c6afe99
Daria <45639752+Saridlin@users.noreply.github.com>
2025-04-14 17:49:09 -0700

    test

--- code/sketch.js
+++ code/sketch.js
@@ -1,8 +1,13 @@
+let canvas;
+
 function setup(){
-  createCanvas(100, 100);
+  canvas = createCanvas(500, 500);
+  canvas.parent('canv-holder');
   background(0);
 }
 
 function draw(){
-
+  stroke(255);
+  strokeWeight(10);
+  point(500, 0);
 }
